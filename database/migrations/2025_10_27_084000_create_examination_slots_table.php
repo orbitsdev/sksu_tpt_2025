2950a55c6be009797362cc2e100b9ecc5b3569a9
Tristan Sarceda <orbinobrian0506@gmail.com>
2025-10-27 16:43:37 +0800

    3123

---
+++ database/migrations/2025_10_27_084000_create_examination_slots_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('examination_slots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('examination_id')->nullable()->constrained()->onDelete('cascade');
+            $table->date('date_of_exam');
+            $table->string('building_name')->nullable();
+            $table->bigInteger('building_name')->nullable();
+            $table->unsignedBigInteger('slots')->default(0)->comment('Total available slots for examinees');
+            $table->unsignedBigInteger('number_of_rooms')->default(0)->comment('Total available rooms for examinees');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('examination_slots');
+    }
+};
